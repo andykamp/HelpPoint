2745a9b0410af5c9b60c869c5f300c86760eac50
andykamp <akampenes@gmail.com>
2017-08-31 08:28:16 +0200

    Added boolean to prevent doble write

--- src/actions/ScrollActions.js
+++ src/actions/ScrollActions.js
@@ -1,4 +1,4 @@
-import { SCROLL } from './types';
+import { SCROLL, PASSIVE_SCROLL , ACTIVE_SCROLL} from './types';
 import firebase from 'firebase';
 
 export const subscribeToScroll = () => {
@@ -7,6 +7,8 @@ export const subscribeToScroll = () => {
   return (dispatch) => {
         ref.on('value', snapshot => {
           dispatch({ type: SCROLL, payload: snapshot.val() });
+          dispatch({ type: PASSIVE_SCROLL });
+
           window.scrollTo(0,snapshot.val());
           console.log('SCROLLSubscribe', snapshot.val());
       });
@@ -18,9 +20,16 @@ export const writeScrollToDatabase = ({ yscroll }) => {
   const ref = firebase.database().ref(`Scroll`);
 
   return (dispatch) => {
-    ref.set({ yscroll }) //sets the value
+    ref.update({ yscroll }) //sets the value
     .then(() => {
       console.log("Scroll Y-coordinates", yscroll);
      });
   };
 };
+
+export const activateScroll = () => {
+  return {
+  type: ACTIVE_SCROLL
+
+};
+}
