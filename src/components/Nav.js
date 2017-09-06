90fb0b3b95f07253ec4705736e8be1a986290bac
Kristoffer Saastad <kristoffer_gs@hotmail.com>
2017-09-06 10:51:32 +0200

    Lagt til search Form

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { } from '../actions';
 import { browserHistory } from 'react-router';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import SearchArea from './Search';
 
 
 
@@ -17,10 +19,27 @@ class Nav extends Component {
 
   render() {
     return (
-      <div className="navbody">
-        <div className="navbody">
-          <img src={require('./images/nav.png')} alt="nav" className="navpage_logo" />
+
+      <div className='outerDivSearch'>
+
+        <div className='helpPointHeader'>
+          <div className = 'hpHeaderBody'>
+            <img />
+          </div>
         </div>
+
+        <div className="navbody">
+          <div className="navbody">
+            <img src={require('./images/nav.png')} alt="nav" className="navpage_logo" />
+          </div>
+        </div>
+
+        <Form inline>
+          <FormControl
+            className = 'formControl'
+            placeholder = 'Search file'
+          />
+        </Form>
       </div>
     );
   }
